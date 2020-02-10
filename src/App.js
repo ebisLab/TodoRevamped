06b895b8d7ae44c0545561280f618c4664d5d9b5
ebisLab <ebaiden@techie.com>
2020-02-10 16:04:48 -0500

    brought in data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
+const taskItem = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+
+
   constructor(){
     super();
     this.state = {
-      todoItem : ''
+      todoItem : taskItem
     }
   }
   // design `App` to be the parent component of your application.
