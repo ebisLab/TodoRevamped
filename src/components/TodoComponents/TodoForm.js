8196735af3ad3555aa96a2d98059aef51ba79cf0
ebisLab <ebaiden@techie.com>
2020-02-11 06:11:02 -0500

    created a change handler for form

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,14 +4,27 @@ export default class TodoForm extends Component {
     constructor(){ //1.
         super(); //2.
         this.state ={ //3
-            task: ''
+            newTask: '' //banana word but must match name value in input
         }
+    }
+
+    handleChange = e =>{
+        //5. update state
+        this.setState({
+        //6. pass in object
+        // [e.target.name]: e.target.value //<---option 1 -- for multiple inputs this is how you controlled all of them
+        newTask: e.target.value // <--option 2. since there's one input
+        })
+
     }
 
     render(){
         return(
             <form>
-                <input placeholder="Add your task here" />
+                <input type="text" name="newTask" 
+                value={this.state.newTask} 
+                placeholder="Add your task here" 
+                onChange={this.handleChange}/>
                 <button>Add</button>
             </form>
         )
